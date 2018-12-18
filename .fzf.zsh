2dd51e56022186ef43bffc1ae867cc842eee808a
5chdn <5chdn@users.noreply.github.com>
2018-12-18 18:55:36 +0100

    Revert "fzf: update installation"
    This reverts commit 66e9545fbc53a030b3f6c34e542b4b8d40900657.

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,14 +1,14 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */root/.fzf/bin* ]]; then
-  export PATH="$PATH:/root/.fzf/bin"
+if [[ ! "$PATH" == */home/user/.fzf/bin* ]]; then
+  export PATH="$PATH:/home/user/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/root/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/home/user/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/root/.fzf/shell/key-bindings.zsh"
+source "/home/user/.fzf/shell/key-bindings.zsh"
 
